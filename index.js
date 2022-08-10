fe8dca0682a03d78b027b5f90dc54c06f1755ad7
Noah Anderson <NoahFoxAnderson@gmail.com>
2022-08-10 12:41:50 -0700

    Added key listener

--- index.js
+++ index.js
@@ -2,9 +2,63 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
+move(character).withArrowKeys(100, 250, moveCharacter)
+function moveCharacter(direction, x, y){
+    if(direction === 'west'){
+        x--
+    }
+    if(direction === 'north'){
+        y++
+    }
+    if(direction === 'east'){
+        x++
+    }
+    if(direction === 'south'){
+        y--
+    }
+    character.style.left = x+'px'
+    character.style.bottom = y+'px'
+}
+
+setInterval(function(direction, x, y){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+
+setInterval(moveCharacter, 1)
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
